0af5e7570599e60846079965f76dfafa52680285
Yubbini <jjas8548@gmail.com>
2020-12-05 22:50:07 +0900

    test

--- api/views.py
+++ api/views.py
@@ -11,19 +11,14 @@ url = f"https://api.telegram.org/bot{TOKEN}/getUpdates"
 bot = telegram.Bot(token=TOKEN)
 chat_id = bot.getUpdates()[-1].message.chat.id
 
-custom_keyboard=[['/레포추가','/레포확인']]
+custom_keyboard=[['레포추가','레포확인']]
 reply_markup=telegram.ReplyKeyboardMarkup(custom_keyboard)
 #bot.send_message(chat_id=chat_id, text="Custom Keyboard Test", reply_markup=reply_markup)
 
 updater = Updater(token=TOKEN, use_context=True)
 dispatcher = updater.dispatcher
 
-updates = bot.getUpdates() 
-for u in updates :
-    #print(u.message)
-    if u.message.text == "레포확인":
-        chat_id = bot.getUpdates()[-1].message.chat.id
-        bot.sendMessage(chat_id=chat_id, text='별명을 선택해주세요')
+updates = bot.getUpdates()
 
 def start(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
@@ -32,10 +27,10 @@ def repoStatus(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text='your repo list')
 
 start_handler = CommandHandler('start', start)
-repoStatus_handler = CommandHandler('레포확인', repoStatus)
+repoStatus_handler = CommandHandler('repo', repoStatus)
 
-dispatcher.add_handler(repoStatus_handler)
 dispatcher.add_handler(start_handler)
+dispatcher.add_handler(repoStatus_handler)
 
 updater.start_polling()
 
